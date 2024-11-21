cd4223673f5f9c0488726313644e57973aada2e9
Samuel Serrato <samysd12@hotmail.com>
2024-11-21 12:45:27 -0600

    fix window

--- app/(auth)/complete-registration/page.tsx
+++ app/(auth)/complete-registration/page.tsx
@@ -1,79 +1,97 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
-import jwt from "jsonwebtoken"
 import { apiClient } from '@/lib/apiClient';
-import CustomInput from '@/components/CustomInput';
 import { Form } from '@/components/ui/form';
 import { completeRegistrationSchema } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import CompleteRegistrationInput from '@/components/CompleteRegistrationInput';
 import { Loader2 } from 'lucide-react';
 
-
 const CompleteRegistration = () => {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
-    const formSchema = completeRegistrationSchema()
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [googleId, setGoogleId] = useState<string | null>(null);
+  const [email, setEmail] = useState<string | null>(null);
 
+  const formSchema = completeRegistrationSchema();
+
+  useEffect(() => {
+    // Solo se ejecuta en el cliente
     const params = new URLSearchParams(window.location.search);
-    console.log(params)
-    const googleId = params.get("googleId")
-    const email = params.get("email")
+    setGoogleId(params.get('googleId'));
+    setEmail(params.get('email'));
+  }, []);
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-        username: "",
-        first_name: "",
-        last_name: "",
-        email: email ? email : "",
-        },
-    })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      first_name: '',
+      last_name: '',
+      email: email || '',
+    },
+  });
 
-    if (!email || !googleId) return <div>No hay params</div>
+  if (!googleId || !email) {
+    return <div>Missing parameters. Please try again.</div>;
+  }
 
-    const onSubmit = async (data: any) => {
-      try {
-        setIsLoading(true)
-        const response = await apiClient.post('/complete-registration', {
-          ...data,
-          googleId,
-        });
-    
-        if (response.data.token) {
-          localStorage.setItem('token', response.data.token);
-          router.push('/');
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false)
+  const onSubmit = async (data: any) => {
+    try {
+      setIsLoading(true);
+      const response = await apiClient.post('/complete-registration', {
+        ...data,
+        googleId,
+      });
+
+      if (response.data.token) {
+        localStorage.setItem('token', response.data.token);
+        router.push('/');
       }
-    };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="flex gap-4">
-          <CompleteRegistrationInput control={form.control} name='first_name' label="First Name" placeholder='Enter your first name' />
-          <CompleteRegistrationInput control={form.control} name='last_name' label="Last Name" placeholder='Enter your last name' />
-          </div>
-
-        <CompleteRegistrationInput control={form.control} name='username' label="Username" placeholder='Enter your username' />
-  
-
+        <div className="flex gap-4">
+          <CompleteRegistrationInput
+            control={form.control}
+            name="first_name"
+            label="First Name"
+            placeholder="Enter your first name"
+          />
+          <CompleteRegistrationInput
+            control={form.control}
+            name="last_name"
+            label="Last Name"
+            placeholder="Enter your last name"
+          />
+        </div>
+        <CompleteRegistrationInput
+          control={form.control}
+          name="username"
+          label="Username"
+          placeholder="Enter your username"
+        />
         <div className="flex flex-col gap-4">
           <Button type="submit" disabled={isLoading} className="form-btn">
             {isLoading ? (
               <>
-                <Loader2 size={20} className="animate-spin" /> &nbsp;
-                Loading...
+                <Loader2 size={20} className="animate-spin" /> &nbsp; Loading...
               </>
-            ) : "Complete Registration"}
+            ) : (
+              'Complete Registration'
+            )}
           </Button>
         </div>
       </form>
