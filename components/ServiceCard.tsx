a1290602be23bfbbdde2af3b682d466c10adb4c1
Samuel Serrato <samysd12@hotmail.com>
2024-11-20 22:47:15 -0600

    fix final ahora si

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -8,25 +8,25 @@ import TransactionForm from './TransactionForm'
 const ServiceCard = ({tittle} : {tittle: string}) => {
   return (
     <Dialog>
-      <DialogTrigger className='w-[200px] h-[150px] p-5 rounded-md bg-bank-gradient text-white font-bold hover:scale-110 transition-all' >
+      <DialogTrigger className='w-[150px] h-[100px] md:w-[200px] md:h-[150px] p-3 md:p-5 rounded-md bg-bank-gradient text-white font-bold hover:scale-110 transition-all' >
         <div className='flex flex-col justify-center items-center gap-5'>
           <h2 className='text-xl'>{tittle}</h2>
           {tittle === "Water"
-            ? <LuDroplets className='text-5xl'/>
+            ? <LuDroplets className='text-3xl md:text-5xl'/>
             : tittle === "Light"
-              ? <LuLightbulb className='text-5xl'/>
+              ? <LuLightbulb className='text-3xl md:text-5xl'/>
               : tittle === "Internet"
-                ? <LuWifi className='text-5xl'/>
+                ? <LuWifi className='text-3xl md:text-5xl'/>
                 : tittle === "Gas"
-                  ? <FaBurn className='text-5xl'/>
+                  ? <FaBurn className='text-3xl md:text-5xl'/>
                   : tittle === "Phone"
-                    ? <LuPhone className='text-5xl'/>
+                    ? <LuPhone className='text-3xl md:text-5xl'/>
                     : tittle === "Television"
-                      ? <LuTv2 className='text-5xl'/>
+                      ? <LuTv2 className='text-3xl md:text-5xl'/>
                       : tittle === "Insurance"
-                        ? <LuLock className='text-5xl'/>
+                        ? <LuLock className='text-3xl md:text-5xl'/>
                         : tittle === "Air time"
-                          ? <LuMessageCircle className='text-5xl'/>
+                          ? <LuMessageCircle className='text-3xl md:text-5xl'/>
                           : tittle === ""
           }
           
