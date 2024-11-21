5289d385cb9b2c004aa05746657d80076f3040be
Samuel Serrato <samysd12@hotmail.com>
2024-11-21 11:58:11 -0600

    fix loader

--- app/(auth)/complete-registration/page.tsx
+++ app/(auth)/complete-registration/page.tsx
@@ -11,6 +11,7 @@ import { Form } from '@/components/ui/form';
 import { completeRegistrationSchema } from '@/lib/utils';
 import { useRouter } from 'next/router';
 import CompleteRegistrationInput from '@/components/CompleteRegistrationInput';
+import { Loader2 } from 'lucide-react';
 
 
 const CompleteRegistration = () => {
@@ -50,27 +51,27 @@ const CompleteRegistration = () => {
 
   return (
     <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <div className="flex gap-4">
-                <CompleteRegistrationInput control={form.control} name='first_name' label="First Name" placeholder='Enter your first name' />
-                <CompleteRegistrationInput control={form.control} name='last_name' label="Last Name" placeholder='Enter your last name' />
-                </div>
-    
-              <CompleteRegistrationInput control={form.control} name='username' label="Username" placeholder='Enter your username' />
-        
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="flex gap-4">
+          <CompleteRegistrationInput control={form.control} name='first_name' label="First Name" placeholder='Enter your first name' />
+          <CompleteRegistrationInput control={form.control} name='last_name' label="Last Name" placeholder='Enter your last name' />
+          </div>
 
-              <div className="flex flex-col gap-4">
-                <Button type="submit" disabled={isLoading} className="form-btn">
-                  {isLoading ? (
-                    <>
-                      <Loader2 size={20} className="animate-spin" /> &nbsp;
-                      Loading...
-                    </>
-                  ) : "Complete Registration"}
-                </Button>
-              </div>
-            </form>
-          </Form>
+        <CompleteRegistrationInput control={form.control} name='username' label="Username" placeholder='Enter your username' />
+  
+
+        <div className="flex flex-col gap-4">
+          <Button type="submit" disabled={isLoading} className="form-btn">
+            {isLoading ? (
+              <>
+                <Loader2 size={20} className="animate-spin" /> &nbsp;
+                Loading...
+              </>
+            ) : "Complete Registration"}
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 };
 
