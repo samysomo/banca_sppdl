57d93dc2ee1c362f3c4dde1945f38026c311b6ff
Samuel Serrato <samysd12@hotmail.com>
2024-11-21 15:03:38 -0600

    form styles

--- app/(auth)/complete-registration/page.tsx
+++ app/(auth)/complete-registration/page.tsx
@@ -11,6 +11,8 @@ import { completeRegistrationSchema } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import CompleteRegistrationInput from '@/components/CompleteRegistrationInput';
 import { Loader2 } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const CompleteRegistration = () => {
   const router = useRouter();
@@ -62,6 +64,28 @@ const CompleteRegistration = () => {
   }
 
   return (
+    <section className='flex-center size-full max-sm:px-6'>
+    <div className="auth-form">
+      <header className='flex flex-col gap-5 md:gap-8'>
+          <Link href="/" className="cursor-pointer flex items-center gap-1">
+            <Image 
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">SPPDL</h1>
+          </Link>
+
+          <div className="flex flex-col gap-1 md:gap-3">
+            <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+              Complete Registration
+              <p className="text-16 font-normal text-gray-600">
+                Please enter your details
+              </p>  
+            </h1>
+          </div>
+      </header>
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <div className="flex gap-4">
@@ -97,6 +121,8 @@ const CompleteRegistration = () => {
         </div>
       </form>
     </Form>
+    </div>
+    </section>
   );
 };
 
